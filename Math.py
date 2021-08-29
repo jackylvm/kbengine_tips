a27350bfa9ba7386de9f2b4d5597eaeb3086bbd5
jacky <jackylvm@foxmail.com>
2021-08-29 21:52:55 +0800

    增加Vector2和Vector4

--- Math.py
+++ Math.py
@@ -9,6 +9,38 @@
 #
 # -----------------------------------------------------
 
+class Vector2():
+    """描述和管理2D空间的向量.
+
+    其中有x,y两个属性代表不同的轴向.
+    """
+
+    def __init__(self, x, y):
+        """"""
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self):
+        """"""
+        return self._x
+
+    @x.setter
+    def x(self, x):
+        """"""
+        self._x = x
+
+    @property
+    def y(self):
+        """"""
+        return self._y
+
+    @y.setter
+    def y(self, y):
+        """"""
+        self._y = y
+
+
 class Vector3():
     """描述和管理3D空间的向量.
 
@@ -20,6 +52,60 @@ class Vector3():
         self._x = x
         self._y = y
         self._z = z
+
+    @property
+    def x(self):
+        """"""
+        return self._x
+
+    @x.setter
+    def x(self, x):
+        """"""
+        self._x = x
+
+    @property
+    def y(self):
+        """"""
+        return self._y
+
+    @y.setter
+    def y(self, y):
+        """"""
+        self._y = y
+
+    @property
+    def z(self):
+        """"""
+        return self._z
+
+    @z.setter
+    def z(self, z):
+        """"""
+        self._z = z
+
+
+class Vector4():
+    """描述和管理4D空间的向量.
+
+    其中有a,x,y,z三个属性代表不同的轴向.
+    """
+
+    def __init__(self, a, x, y, z):
+        """"""
+        self._a = a
+        self._x = x
+        self._y = y
+        self._z = z
+
+    @property
+    def a(self):
+        """"""
+        return self._a
+
+    @a.setter
+    def a(self, a):
+        """"""
+        self._a = a
 
     @property
     def x(self):
