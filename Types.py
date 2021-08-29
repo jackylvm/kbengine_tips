10f7a9a302be61f7994243c3eab89b0a9e0a088f
jacky <jackylvm@foxmail.com>
2021-08-29 21:51:00 +0800

    可使用的基本类型

---
+++ Types.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File Created: 2021-08-29 21:45:20
+# Author: Jacky (jackylvm@foxmail.com>)
+# -----
+# HISTORY:
+# Date      			By			Comments
+# --------------------	---------	-------------------
+#
+# -----------------------------------------------------
+
+class UINT8(int):
+    """"""
+
+
+class UINT16(int):
+    """"""
+
+
+class UINT32(int):
+    """"""
+
+
+class UINT64(int):
+    """"""
+
+
+class INT8(int):
+    """"""
+
+
+class INT16(int):
+    """"""
+
+
+class INT32(int):
+    """"""
+
+
+class INT64(int):
+    """"""
+
+
+class FLOAT(float):
+    """"""
+
+
+class DOUBLE(float):
+    """"""
+
+
+class STRING(str):
+    """"""
+
+
+class UNICODE(str):
+    """"""
+
+
+class PYTHON(object):
+    """"""
+
+
+class PY_DICT(dict):
+    """"""
+
+
+class PY_TUPLE(tuple):
+    """"""
+
+
+class PY_LIST(list):
+    """"""
+
+
+class ENTITYCALL(object):
+    """"""
+
+
+class BLOB(object):
+    """"""
