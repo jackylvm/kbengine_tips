221f3c0e921cb8106741c0a2bbfeed04f53b2285
jacky <jackylvm@foxmail.com>
2021-08-29 21:39:52 +0800

    公共数学方法

---
+++ Math.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File Created: 2021-08-29 21:23:54
+# Author: Jacky (jackylvm@foxmail.com>)
+# -----
+# HISTORY:
+# Date      			By			Comments
+# --------------------	---------	-------------------
+#
+# -----------------------------------------------------
+
+class Vector3():
+    """描述和管理3D空间的向量.
+
+    其中有x,y,z三个属性代表不同的轴向.
+    """
+
+    def __init__(self, x, y, z):
+        """"""
+        self._x = x
+        self._y = y
+        self._z = z
+
+    @property
+    def x(self):
+        """"""
+        return self._x
+
+    @x.setter
+    def x(self, x):
+        """"""
+        self._x = x
+
+    @property
+    def y(self):
+        """"""
+        return self._y
+
+    @y.setter
+    def y(self, y):
+        """"""
+        self._y = y
+
+    @property
+    def z(self):
+        """"""
+        return self._z
+
+    @z.setter
+    def z(self, z):
+        """"""
+        self._z = z
