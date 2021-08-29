250496fec9b004e57ee05e3016c4ad67801edcfd
jacky <jackylvm@foxmail.com>
2021-08-29 20:31:36 +0800

    add python setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='kbengine_tips',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/jackylvm/kbengine_tips',
+    license='GPL 3.0',
+    author='jacky',
+    author_email='jackylvm@foxmail.com',
+    description='KBEngine编码提示'
+)
