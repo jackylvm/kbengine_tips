e3cb968bf37a117f49c6ae369e43b3e0213fa88e
jacky <jackylvm@foxmail.com>
2021-09-21 10:38:54 +0800

    调整文件夹结构

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf8') as f:
+    LONG_DESCRIPTION = f.read()
 
 setup(
     name='kbengine_tips',
     version='1.2.0',
-    packages=[''],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    package_data={
+        ".": ["*.MD"],
+    },
     url='https://github.com/jackylvm/kbengine_tips',
     license='GPL 3.0',
     author='jacky',
     author_email='jackylvm@foxmail.com',
-    description='KBEngine编码提示'
+    description='KBEngine编码提示',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown'
 )
---
+++ src/kbengine_tips/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File Created: 2021-09-21 09:54:01
+# Author: Jacky (jackylvm@foxmail.com>)
+# -----
+# HISTORY:
+# Date      			By			Comments
+# --------------------	---------	-------------------
+#
+# -----------------------------------------------------
