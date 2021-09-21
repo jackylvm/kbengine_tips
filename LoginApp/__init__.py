60a24a6f513682a3f76abe207bbdf14e6659c136
jacky <jackylvm@foxmail.com>
2021-09-21 08:58:35 +0800

    LoginApp编写完成

---
+++ LoginApp/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File Created: 2021-08-29 19:22:08
+# Author: Jacky (jackylvm@foxmail.com>)
+# -----
+# HISTORY:
+# Date      			By			Comments
+# --------------------	---------	-------------------
+#
+# -----------------------------------------------------
+# Loginapp进程主要负责处理实体的注册与登陆请求.
+# 注意:该进程脚本实现在scripts/login目录中.
