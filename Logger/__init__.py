be1e15febe778d96f8123cca81a4954691719f7f
jacky <jackylvm@foxmail.com>
2021-09-21 09:22:27 +0800

    Logger编写完成

---
+++ Logger/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File Created: 2021-08-29 19:22:08
+# Author: Jacky (jackylvm@foxmail.com>)
+# -----
+# HISTORY:
+# Date      			By			Comments
+# --------------------	---------	-------------------
+#
+# -----------------------------------------------------
+# Logger进程主要负责收集KBEngine服务器所输出的日志.
+# 注意:该进程脚本实现在scripts/logger目录中.
