a5b4a4f85e422bd79b4a9c0814d428481473ae0f
jacky <jackylvm@foxmail.com>
2021-09-21 09:04:02 +0800

    DBMgr编写完成

---
+++ DBMgr/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File Created: 2021-08-29 19:22:08
+# Author: Jacky (jackylvm@foxmail.com>)
+# -----
+# HISTORY:
+# Date      			By			Comments
+# --------------------	---------	-------------------
+#
+# -----------------------------------------------------
+# Dbmgr进程主要负责处理实体数据的存储与实体数据的加载查询等等.
+# 注意:该进程脚本实现在scripts/db目录中.
